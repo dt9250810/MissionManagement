7f59be5e44ddcaeb0e4594176b289a7d0c449602
bruce <dt9250810@gmail.com>
2019-07-30 21:25:08 +0800

    step 11: order by created_at & rspec

---
+++ spec/features/mission_order_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+feature "任務依建立時間排序" do
+  scenario "依建立時間排序" do
+    visit missions_path
+    expect(page).to have_http_status(200)
+
+    # mission_order = Mission.order('created_at DESC').map { |mission| page.body.index(mission.id) }
+    mission_order = Mission.order('created_at DESC').limit(5).map { |mission| mission.id }
+    mission_order2 = Mission.all.limit(5).map { |mission| mission.id }
+    expect(mission_order).to eq(mission_order2)
+
+  end
+end
