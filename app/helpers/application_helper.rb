1dc49f737ceaacf25e89c9975922c81f93af317e
bruce <dt9250810@gmail.com>
2019-07-30 22:57:02 +0800

    step 12: mission model加上基本驗證, 補齊 _form 填寫欄位, view顯示驗證錯誤訊息

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def error_message_for(obj, name)
+    if obj && obj.errors.any?
+      result = ''
+      obj.errors.full_messages_for(name).each do |message|
+        result += message + ', '
+      end 
+    end
+    raw result.chomp! ', ' if result.present?
+  end
 end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,3 +1,16 @@
 class Mission < ApplicationRecord
   default_scope { order("created_at DESC") }
+  validates_presence_of :title, :context, :status, :priority, :start_time, :end_time
+  validate :datetime_validate
+
+  STATUS_OPTIONS = [%w(待處理 1), %w(進行中 2), %w(已完成 3)].freeze
+  PRIORITY_OPTIONS = [%w(高 1), %w(中 2), %w(低 3)].freeze
+
+
+  private
+  def datetime_validate
+    if start_time > end_time
+      errors[:start_time] << '不可大於結束時間'
+    end
+  end
 end
