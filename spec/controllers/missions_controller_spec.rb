9151b6897b081996e460568287d0ee07b916b05f
bruce <dt9250810@gmail.com>
2019-08-01 20:37:38 +0800

    rspec for controller & helper

---
+++ spec/controllers/missions_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe MissionsController, type: :controller do
+  before(:all) do
+    @mission = Mission.create(title: "rspec_test1", context: "this is testing context.")
+  end
+
+  it "#index" do
+    get :index
+    expect(response).to have_http_status(200)
+  end
+
+  it "#new" do
+    get :new
+    expect(response).to have_http_status(200)
+  end
+
+  it "#edit" do
+    get :edit, params: { id: @mission[:id] }
+    expect(response).to have_http_status(200)
+  end
+
+  describe "#create" do
+    before(:all) do
+      @mission_params = { title: "title", context: "context" }
+    end
+
+    it "creates record" do
+      expect {
+         post :create, params: { mission: @mission_params }
+      }.to change{ Mission.all.size }.by(1)
+    end
+
+    it "redirect on success" do
+      post :create, params: { mission: @mission_params }
+      expect(response).not_to have_http_status(200)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(missions_path)
+    end
+  end
+  
+end
